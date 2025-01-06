01280638e35342301f73e99d6753125d256393dd
rajnn <nikilesh.raj.n@autodesk.com>
2025-01-06 10:57:29 +0530

    multiple declarations

--- Lesson2/main.go
+++ Lesson2/main.go
@@ -19,7 +19,12 @@ func main() {
 	var secondString string = "World"
 	var inferenceTypeCheck = "Nikilesh"
 
+	// Declaring multiple types is also valid
+	myInteger, myFloat, myString := 2, 2.4, "Nikilesh"
+
 	fmt.Printf("%v %.2f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
 	fmt.Printf("%q\n", firstString+" "+secondString)
 	fmt.Println(inferenceTypeCheck)
+	fmt.Println(myInteger, myFloat, myString)
+	fmt.Printf("%T %T %T\n", myInteger, myFloat, myString)
 }
