70e0688aef9d95c6b6b315698eff9d4311950e8f
Nikilesh Raj <nikileshnatraj@gmail.com>
2025-02-24 00:27:00 +0530

    Primes printer

--- Lesson15/main.go
+++ Lesson15/main.go
@@ -25,6 +25,32 @@ func fizzbuzz() {
 	}
 }
 
+func printPrimes(max int) {
+	for n := 2; n < max + 1; n++ {
+		if n == 2 {
+			fmt.Println(n)
+			continue
+		}
+
+		if n % 2 != 0 {
+			isPrime := true
+			for i := 3; i * i < n + 1; i++ {
+				if n % i == 0 {
+					isPrime = false
+					break
+				}
+			}
+
+			if !isPrime {
+				continue
+			}
+
+			fmt.Println(n)
+		}
+	}
+}
+
 func main() {
 	fizzbuzz()
+	printPrimes(10)
 }
