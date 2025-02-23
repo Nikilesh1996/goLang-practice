d168a2f67ba9f43972f618e24fc861696ea21bf2
Nikilesh Raj <nikileshnatraj@gmail.com>
2025-02-24 02:18:29 +0530

    Added spread operation example

--- Lesson19/main.go
+++ Lesson19/main.go
@@ -4,6 +4,27 @@ import (
 	"fmt"
 )
 
+type Cost struct {
+	day int 
+	value float64
+}
+
+func getCostsByDay(costs []Cost) []float64 {
+	costsByDay := []float64{}
+
+	for i := 0; i < len(costs); i++ {
+		cost := costs[i]
+
+		for cost.day >= len(costsByDay) {
+			costsByDay = append(costsByDay, 0.0)			
+		}
+
+		costsByDay[cost.day] += cost.value
+	}
+
+	return costsByDay
+}
+
 func sum(elements ...float64) float64 {
 	total := 0.0
 
@@ -17,4 +38,12 @@ func sum(elements ...float64) float64 {
 func main() {
 	total := sum(1.2, 2.2, 3.2)
 	fmt.Println(total)
+
+	costs := getCostsByDay([]Cost {
+		{1, 2.2},
+		{2, 2.2},
+		{1, 1.1},
+	})
+
+	fmt.Println(costs)
 }
